1c2ea441aee41132a5931f52a5d0f25ad59d3eea
Davbkim0420@gmail.com <Davbkim0420@gmail.com>
2024-10-21 23:39:48 -0400

    proj done

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,42 @@
-import './App.css'
-import Header from './components/Header.tsx'
-import NavBar from './components/Nav.tsx'
-import HomePage from './components/Home'
+import './App.css';
+import Header from './components/Header.tsx';
+import NavBar from './components/Nav.tsx';
+import HomePage from './components/Home';
 import ReferencesPage from "./components/Refrences.tsx";
 import EmployeePage from "./components/Employment.tsx";
 import AchievementPage from "./components/Achievements.tsx";
 import EducationPage from "./components/Education.tsx";
 import Calculator from "./components/Project.tsx";
-import{ createBrowserRouter, Route, Routes, RouterProvider} from "react-router-dom";
+import FooterPage from "./components/Footer.tsx";
+import { createBrowserRouter, Route, Routes, RouterProvider, Link } from "react-router-dom";
 
 const Root = () => {
-    return(
-
+    return (
         <div>
-            <Header/>
-            <NavBar/>
-                <Routes>
-                    <Route path="/" element={<HomePage/>}/>
-                    <Route path="/Employment" element={<EmployeePage/>}/>
-                    <Route path="/References" element ={<ReferencesPage/>}/>
-                    <Route path="/Achievements" element ={<AchievementPage/>}/>
-                    <Route path="/education" element={<EducationPage/>}/>
-                    <Route path="/Project" element = {<Calculator/>}/>
-                 </Routes>
+            <Header />
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/Employment" element={<EmployeePage />} />
+                <Route path="/References" element={<ReferencesPage />} />
+                <Route path="/Achievements" element={<AchievementPage />} />
+                <Route path="/Education" element={<EducationPage />} />
+                <Route path="/Project" element={<Calculator />} />
+                <Route path="/Footer" element={<FooterPage />} />
+            </Routes>
+            <footer>
+                <p>All rights reserved, <Link to="/Footer">© Copyright Credits</Link></p>
+            </footer>
         </div>
-    )
+    );
 }
+
 const router = createBrowserRouter(
-    [{path: "*", Component: Root}]
-)
-export default function App()
-{
-    return(
-        <RouterProvider router={router}/>
-    )
+    [{ path: "*", Component: Root }]
+);
+
+export default function App() {
+    return (
+        <RouterProvider router={router} />
+    );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+import { styled } from 'styled-components';
+
+const Footer = styled.footer`
+    border:black; 
+    padding-bottom: 5%;
+    padding-left: 20%;
+`
+export default function FooterPage(){
+    return (
+        <Footer>
+            <p>https://www.google.com/imgres?q=cu%20boulder%20image%20farrand&
+                imgurl=https%3A%2F%2Fwww.colorado.edu%2Ftheherd%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fsma
+                ll%2Fpublic%2Fcollection-image%2Ffarrand_A</p>
+            <p>https://www.google.com/imgres?q=bu%20image&imgurl=https%3A%2F%2Fwww.ivywise.com%2Fcore%2Fwp-content%2Fuploads%2F2023%2F04%2Fbu-campus.jpg&imgrefu
+                rl=https%3A%2F%2Fwww.ivywise.com%2Fblog%2Fhow-to-get-into-boston-university-all-you-need-to-know%2F&docid=qgSwtog2BA-XiM&tbnid=yI8MrvD3pCiR_M&vet=12a
+                hUKEwiw-o-b_NKIAxXOC3kGHTTAI0gQM3oECAsQAA..i&w=1500&h=1000&hcb=2&ved=2ahUKEwiw-o-b_NKIAxXOC3kGHTTAI0gQM3oECAsQAA</p>
+            <p>https://www.google.com/imgres?q=cu%20boulder%20image%20farrand&
+                imgurl=https%3A%2F%2Fwww.colorado.edu%2Ftheherd%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fsma
+                ll%2Fpublic%2Fcollection-image%2Ffarrand_A</p>
+            <p>https://www.google.com/imgres?q=bu%20image&imgurl=https%3A%2F%2Fwww.ivywise.com%2Fcore%2Fwp-content%2Fuploads%2F2023%2F04%2Fbu-campus.jpg&imgrefu
+                rl=https%3A%2F%2Fwww.ivywise.com%2Fblog%2Fhow-to-get-into-boston-university-all-you-need-to-know%2F&docid=qgSwtog2BA-XiM&tbnid=yI8MrvD3pCiR_M&vet=12a
+                hUKEwiw-o-b_NKIAxXOC3kGHTTAI0gQM3oECAsQAA..i&w=1500&h=1000&hcb=2&ved=2ahUKEwiw-o-b_NKIAxXOC3kGHTTAI0gQM3oECAsQAA</p>
+        </Footer>
+    )
+}
